f9d33873b67ea20b4f07f0b34009c6e0da96bdc2
Gwyneth Pena <gwynethpena@outlook.com>
2021-12-09 20:58:33 -0500

    Fixed github link

--- ApiIsolated/GetCoinJson.cs
+++ ApiIsolated/GetCoinJson.cs
@@ -29,10 +29,11 @@ namespace ApiIsolated
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            // HttpResponseMessage response = await httpClient.GetAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+           
+            prices = await httpClient.GetFromJsonAsync<Coin[]>("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&price_change_percentage=1h");
+            // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h
 
-            prices = await httpClient.GetFromJsonAsync<Coin[]>("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-            
+
             var response = req.CreateResponse(HttpStatusCode.OK);
             await response.WriteAsJsonAsync(prices);
             return response;
--- Client/Pages/FetchData.razor.cs
+++ Client/Pages/FetchData.razor.cs
@@ -2,7 +2,6 @@ using System.Net.Http.Json;
 using BlazorApp.Shared;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR.Client;
-
 namespace BlazorApp.Client
 {
     public class FetchDataBase : ComponentBase
@@ -11,19 +10,15 @@ namespace BlazorApp.Client
 
         [Inject]
         public HttpClient Http { get; set; }
-
-
         protected HubConnection hubConnection;
-
         public bool IsConnected => hubConnection.State == HubConnectionState.Connected;
         protected override async Task OnInitializedAsync()
         {
-
             prices = await Http.GetFromJsonAsync<Coin[]>("/api/GetCoinJson");
 
             hubConnection = new HubConnectionBuilder().WithUrl("https://functioncryptobackend.azurewebsites.net/api").Build();
 
-            hubConnection.On<Coin[]>("updated", (coin) =>
+            hubConnection.On<Coin[]>("updated", (coin) => 
             {
                 prices = coin;
                 StateHasChanged();
@@ -33,8 +28,5 @@ namespace BlazorApp.Client
 
             await hubConnection.StartAsync();
         }
-
-
-
     }
 }
